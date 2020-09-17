9a7346e54dda7b93899b49791b87f487133e920a
adrian <adrianbravo145@gmail.com>
2020-09-17 00:00:30 -0300

    metodo de getPosts y uploadImage

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
-import bodyParser from 'body-parser';
-import cors from 'cors'; 
+import bodyParser from "body-parser";
+import cors from "cors";
 // import { app } from "./server/server";
 import mongoose from "mongoose";
+import fileUpload from "express-fileupload";
 import Server from "./server/server";
 import userRoutes from "./routes/usuario";
 import postRouts from "./routes/post";
@@ -16,34 +17,36 @@ const url = `mongodb://localhost:27017/RedSocial`;
 
 const server = new Server();
 
-
-
 // Body-parser
-server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
 
-// Cors
-server.app.use(cors({origin: true, credentials:true}));
+// FileUpload
+server.app.use(fileUpload());
 
+// Cors
+server.app.use(cors({ origin: true, credentials: true }));
 
 // Rutas de la app
 server.app.use("/user", userRoutes);
 server.app.use("/posts", postRouts);
 
-
 // conect bd de mongo
 mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
     server.start(port, () => {
       console.log(`Servidor corriendo exitosamente en el puerto ${port}`);
     });
     console.log("La conexion a la bd se ha realizado bien");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log("ENTREEEEEEEEE al catch err: ", err);
     throw err;
-});
-
-
-
-
+  });
--- routes/post.ts
+++ routes/post.ts
@@ -1,19 +1,44 @@
 import { Router, Request, Response } from "express";
+import { FileUpload } from "../interfaces/file";
 import { verificaToken } from "../middlewares/auth";
 import { Post } from "../models/post.model";
 
 const postRouts = Router();
 
-// nuevo post
+// Obtener post
+postRouts.get("/", async (req: any, res: Response) => {
+  let pagina = Number(req.query.pagina) || 1;
+  let skip = pagina - 1;
+  skip = skip * 10;
+
+  // const post = await Post.find().sort({mensaje: 'asc'}).exec();
+  const post = await Post.find()
+    .sort({ _id: "desc" })
+    .limit(10)
+    .skip(skip)
+    .populate("usuario", "avatar nombre email ")
+    .exec();
+
+  const postNumbers = await Post.count({});
+
+  return res.json({
+    ok: true,
+    post: post,
+    pagina,
+    skip,
+    postNumbers,
+  });
+});
+
+// crear post
 postRouts.post("/", verificaToken, (req: any, res: Response) => {
   const body = req.body;
   body.usuario = req.usuario._id;
 
   Post.create(body)
     .then(async (post) => {
-    
-    //  await post.populate("usuario", '-password').execPopulate(); // relacion con la tabla usuarios en la bd, esto hace referecia y toma esta relacion q esta en el modelo
-      await post.populate("usuario", 'avatar nombre email ').execPopulate(); // relacion con la tabla usuarios en la bd, esto hace referecia y toma esta relacion q esta en el modelo
+      //  await post.populate("usuario", '-password').execPopulate(); // relacion con la tabla usuarios en la bd, esto hace referecia y toma esta relacion q esta en el modelo
+      await post.populate("usuario", "avatar nombre email ").execPopulate(); // relacion con la tabla usuarios en la bd, esto hace referecia y toma esta relacion q esta en el modelo
       return res.json({
         ok: true,
         body: body,
@@ -26,6 +51,46 @@ postRouts.post("/", verificaToken, (req: any, res: Response) => {
         err: err,
       });
     });
+});
+
+// subir archivos
+postRouts.put("/upload", [verificaToken], (req: any, res: Response) => {
+  if (!req.files) {
+    return res.status(400).json({
+      ok: false,
+      mensaje: "No selecciono ningun archivo",
+      errors: { message: "Debe seleccionar una imagen" },
+    });
+  }
+
+  const file: FileUpload = req.files.image; //imagen es el nombre que esta en el postman
+
+  if (!file) {
+    return res.status(400).json({
+      ok: false,
+      mensaje: "No selecciono ningun archivo",
+      errors: { message: "Debe seleccionar una imagen - image" },
+    });
+  }
+
+  // validar que siempre sea una imagen
+  console.log( "file.mimetype.includes(image): ",file.mimetype.includes("image")
+  );
+  if (!file.mimetype.includes("image")) {
+    return res.status(400).json({
+      ok: false,
+      mensaje: "No selecciono ningun archivo",
+      fileMineType: file.mimetype,
+      errors: { message: "Debe seleccionar una imagen - image" },
+    });
+  }
+
+  return res.status(200).json({
+    ok: true,
+    mensaje: "Se subio archivo",
+    fileMineType: file.mimetype,
+    file: file.mimetype.includes("image"),
+  });
 });
 
 export default postRouts;
